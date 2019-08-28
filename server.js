99aac272406fb43c24904057cbffa1dffa456c1b
terry <tc216997@gmail.com>
2019-08-28 14:57:13 +0800

    t

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const favicon = require('express-favicon');
 const path = require('path');
-const port = process.env.PORT || port;
+const port = process.env.PORT || 8080;
 const app = express();
 app.use(favicon(__dirname + '/public/favicon.ico'));
 // the __dirname is the current directory from where the script is running
@@ -11,6 +11,6 @@ app.get('/ping', function (req, res) {
  return res.send('pong');
 });
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 app.listen(port);
